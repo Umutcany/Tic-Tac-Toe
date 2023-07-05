776130211ab3fd177cc8f15fdccef081157c1bff
Umut Can Yilmaz <ylmazumutcan49@gmail.com>
2023-07-05 16:41:23 +0300

    wrongone

--- app.js
+++ app.js
@@ -7,14 +7,34 @@ const startCells = [
     "", "", "", "", "", "",  "", "", ""
 ]
 
+let go = "circle"
+infoDisplay.textContent="İlk Yuvarlak Başlar"
+
 
 function createBoard() {
 
-const cellElements = startCells.map((cell, index) => {
+const cellElements = startCells.map((_cell, index) => {
     const cellElement = document.createElement("div");
     cellElement.classList.add("square")
+    cellElement.id = index
+    cellElement.addEventListener("click", addGo)
+    gameboard.append(cellElement)
     return cellElement;
   })
 
 }
-  
+
+createBoard()
+
+
+
+function addGo(e) {
+
+    const goDisplay = document.createElement('div')
+    goDisplay.classList.add(go)
+    e.target.append(goDisplay)
+    go = go==="circle" ?"cross" : "circle"
+    infoDisplay.textContent =`şimdi sıra ${go} 'da`
+    e.target.removeEventListener("click", addGo)
+}
+
